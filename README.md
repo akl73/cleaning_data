bade00c26273bb087ac89b467e863ff152e0fd2f
Anna Lenkiewicz <anna.lenkiewicz@hotmail.com>
2016-05-01 10:00:17 +0100

    Getting and Cleaning Data Project Assignment

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # cleaning_data
-Getting and Cleaning Data Course Project
+## Getting and Cleaning Data Course Project
+
+The repo contains the following files:
+run_analysis.R - script which creates tidy data set
+activity_data.csv - clean data set
+activity_summary.csv - summary data set of activity_data.csv
+CodeBook.md - a code book describing the operations performed on the raw data to generate activity_data.csv and activity_summary.csv
+
+run_analysis.R can be run in R or RStudio.
+
+run_analysis.R requires following packages:
+data.table
+plyr
+dplyr
+They need to be installed prior to running the script.
+The script will load them in the first step.
+
+The raw data will be downloaded from the following location:
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+to the ./data folder (it will be created if does not exist).
+
+Then the relevant files required to produce the tidy data set will be extracted from the downloaded zip.
+
+The next steps are to consolidate test and train data, add column names, add activity values,
+subset only variables relevant to this project and rearrange columns so factors (activity and subjectid)
+as at the front.
+Also column names as tidied following Getting and Cleaning Data Course guidelines.
+
+In the next step the summary data set is produced from the tidy data set - means of all variables
+grouped by activity and subjectid.
+
+In the last step both data sets are saved to the csv files.
+
+More information on transforms performed can be found in CodeBook.md and also there are comments in run_analysis.R.
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,105 @@
+library(data.table)
+library(plyr)
+library(dplyr)
+
+# load_files from the website
+if (!file.exists("./data")){dir.create("data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl, destfile = "./data/human_activity.zip", method = "curl")
+
+# unzip the files we are interested in
+unzip("./data/human_activity.zip", files=c("UCI HAR Dataset/activity_labels.txt", "UCI HAR Dataset/features.txt", 
+                                           "UCI HAR Dataset/test/X_test.txt", "UCI HAR Dataset/test/y_test.txt" ,
+                                           "UCI HAR Dataset/train/X_train.txt", "UCI HAR Dataset/train/y_train.txt",
+                                           "UCI HAR Dataset/test/subject_test.txt", "UCI HAR Dataset/train/subject_train.txt"),
+                                    exdir = "./data")
+
+# load files into data tables
+activities <- read.table("./data/UCI HAR Dataset/activity_labels.txt", sep = "", header = FALSE)
+activitiesDT <- data.table(activities)
+setnames(activitiesDT, c("activityid", "activity"))
+rm(activities)
+
+features <- read.table("./data/UCI HAR Dataset/features.txt", sep = "", header = FALSE, stringsAsFactors = FALSE)
+colnames <- features[, 2]
+rm(features)
+
+testData <- read.table("./data/UCI HAR Dataset/test/X_test.txt", sep = "", header = FALSE, stringsAsFactors = FALSE)
+testDataDT <- data.table(testData)
+rm(testData)
+
+testActivities <- read.table("./data/UCI HAR Dataset/test/y_test.txt", sep = "", header = FALSE)
+testActivitiesDT <- data.table(testActivities)
+rm(testActivities)
+
+testSubjects <- read.table("./data/UCI HAR Dataset/test/subject_test.txt", sep = "", header = FALSE)
+testSubjectsDT <- data.table(testSubjects)
+rm(testSubjects)
+
+trainData <- read.table("./data/UCI HAR Dataset/train/X_train.txt", sep = "", header = FALSE)
+trainDataDT <- data.table(trainData)
+rm(trainData)
+
+trainActivities <- read.table("./data/UCI HAR Dataset/train/y_train.txt", sep = "", header = FALSE)
+trainActivitiesDT <- data.table(trainActivities)
+rm(trainActivities)
+
+trainSubjects <- read.table("./data/UCI HAR Dataset/train/subject_train.txt", sep = "", header = FALSE)
+trainSubjectsDT <- data.table(trainSubjects)
+rm(trainSubjects)
+
+# add column names to x_test.txt and x_train.txt from features.txt
+setnames(testDataDT, colnames)
+setnames(trainDataDT, colnames)
+
+
+# consolidate test data, activities and subjects
+testDataDT[, activityid:= testActivitiesDT$V1]
+testDataDT[, subjectid := testSubjectsDT$V1]
+
+# consolidate train data, activities and subjects
+trainDataDT[, activityid:= trainActivitiesDT$V1]
+trainDataDT[, subjectid := trainSubjectsDT$V1]
+
+# create one data table from train and test data
+allDataDT <- tbl_df(rbind(testDataDT, trainDataDT))
+
+# join activity labels 
+allDataActivityDT <- join(allDataDT, activitiesDT)
+
+# change subject into factor
+allDataActivityDT$subjectid <- as.factor(allDataActivityDT$subjectid)
+
+# extract only means and standard deviation plus activities and subjects
+pattern <- "mean|std"
+meansAndSd <- grepl(pattern,tolower(names(allDataActivityDT)))
+meansAndSd[c(563, 564)] = TRUE # add activity and subjectid to the logical extractor vector
+finalDataSet <- allDataActivityDT[, meansAndSd, with = FALSE]
+
+# cleanup column names
+
+names(finalDataSet) <- gsub("\\(|\\)|,|-", "", names(finalDataSet))
+names(finalDataSet) <- gsub("^t", "time", names(finalDataSet))
+names(finalDataSet) <- gsub("^f", "freq", names(finalDataSet))
+names(finalDataSet) <- gsub("Acc", "acceleration", names(finalDataSet))
+names(finalDataSet) <- gsub("Mag", "magnitude", names(finalDataSet))
+names(finalDataSet) <- tolower(names(finalDataSet)) 
+setcolorder(finalDataSet, c(length(finalDataSet), 1:(length(finalDataSet)-1))) # move subjectid as the first column
+setcolorder(finalDataSet, c(length(finalDataSet), 1:(length(finalDataSet)-1))) # move activity as the first column
+finalDataSet <- finalDataSet %>% arrange(activity, subjectid)
+
+# create a summary data set
+
+summaryData <- finalDataSet %>%
+               group_by(activity, subjectid) %>%
+               summarise_each(funs(mean)) %>%
+               arrange(activity, subjectid)
+
+# save clean data set
+
+write.csv(finalDataSet, file = "./data/activity_data.csv")
+
+
+# save summary data set
+
+write.csv(summaryData, file = "./data/activity_summary.csv")
